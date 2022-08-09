2b5e15078b9daa9df03dff95d272cd06db8bfd7d
gonzalo quiroga <gtquiroga@uc.cl>
2022-08-09 18:27:04 +0300

    feat(Exchange): add contract and constructor with deploy test

---
+++ test/Exchange.js
@@ -0,0 +1,36 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe('Exchange', () => {
+    let deployer, feeAccount, exchange
+    const feePercent = 10
+
+    beforeEach(async () => {
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
+        feeAccount = accounts[1]
+
+        const Exchange = await ethers.getContractFactory('Exchange')
+        exchange = await Exchange.deploy(feeAccount.address, feePercent)
+
+    })
+
+    describe('Deployment', () => {
+
+        it('track fee account', async () => {
+            expect(await exchange.feeAccount()).to.equal(feeAccount.address)
+        })
+
+        it('track fee percent', async () => {
+            expect(await exchange.feePercent()).to.equal(feePercent)
+        })
+    
+
+    })
+
+    
+})
