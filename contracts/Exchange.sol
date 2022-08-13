86150fd1520f8b135e4b70694fe0a7516503b9a0
gonzalo quiroga <gtquiroga@uc.cl>
2022-08-13 13:46:45 +0300

    feat(Exchange): add cancelrder function with test

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -22,6 +22,7 @@ contract Exchange {
 
     mapping(address => mapping(address => uint256)) public tokens;
     mapping(uint256 => _Order) public orders;
+    mapping(uint256 => bool) public orderCancelled;
 
 
     event Deposit(
@@ -39,6 +40,16 @@ contract Exchange {
     );
 
     event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+
+    event Cancel(
         uint256 id,
         address user,
         address tokenGet,
@@ -93,6 +104,26 @@ contract Exchange {
             _amountGet,
             _tokenGive,
             _amountGive,
+            block.timestamp
+        );
+    }
+
+    function cancelOrder(uint256 _id) public {
+        _Order storage _order = orders[_id];
+        
+        require(address(_order.user) == msg.sender);
+        require(_order.id == _id);
+
+        orderCancelled[_id] = true;
+
+
+        emit Cancel(
+            _order.id,
+            msg.sender,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive,
             block.timestamp
         );
     }
