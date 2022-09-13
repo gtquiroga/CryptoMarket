cc62ff3ee880087a6080511da023bd2da64cd434
gonzalo quiroga <gtquiroga@uc.cl>
2022-09-13 12:43:50 -0300

    feat(exchange): add seeds

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,9 +1,24 @@
-
 async function main() {
   const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
-  await token.deployed();
-  console.log(`Token deply to : ${token.address}`)
+  const Exchange = await ethers.getContractFactory("Exchange");
+
+  const accounts = await ethers.getSigners()
+
+  const tkn = await  Token.deploy('My Token', 'TKN', '1000000')
+  await tkn.deployed();
+  console.log(`TKN deploy to : ${tkn.address}`)
+
+  const mETH = await  Token.deploy('Mock ETH', 'mETH', '1000000')
+  await mETH.deployed();
+  console.log(`mETH deploy to : ${mETH.address}`)
+
+  const mDAI = await  Token.deploy('Mock DAI', 'mDAI', '1000000')
+  await mDAI.deployed();
+  console.log(`mDAI deploy to : ${mDAI.address}`)
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10)
+  await exchange.deployed()
+  console.log(`Exchange deploy to : ${exchange.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
