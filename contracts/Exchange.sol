2ea3dd6b4d1865d098a7c10ab1f76802c2ff3360
gonzalo quiroga <gtquiroga@uc.cl>
2022-09-13 11:09:56 -0300

    feat(exchange): create fillOrder and trade function with test

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -23,6 +23,7 @@ contract Exchange {
     mapping(address => mapping(address => uint256)) public tokens;
     mapping(uint256 => _Order) public orders;
     mapping(uint256 => bool) public orderCancelled;
+    mapping(uint256 => bool) public orderFilled;
 
 
     event Deposit(
@@ -59,6 +60,17 @@ contract Exchange {
         uint256 timestamp
     );
 
+    event Trade(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        address creator,
+        uint256 timestamp
+    );
+
     constructor (address _feeAccount, uint256 _feePercent){
         feeAccount = _feeAccount;
         feePercent = _feePercent;
@@ -85,7 +97,7 @@ contract Exchange {
     function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive)public {
         require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
 
-        orderCount = orderCount + 1;
+        orderCount ++;
 
         orders[orderCount] = _Order(
             orderCount,
@@ -126,6 +138,58 @@ contract Exchange {
             _order.amountGive,
             block.timestamp
         );
+    }
+
+    function fillOrder(uint256 _id) public{
+        require(_id > 0 && _id <= orderCount, "Order does not exist");
+
+        require(!orderCancelled[_id]);
+        require(!orderFilled[_id]);
+
+        _Order storage _order = orders[_id];
+
+        _trade(
+            _order.id,
+            _order.user,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive
+        );
+
+        orderFilled[_order.id] = true;
+    }
+
+    function _trade(
+        uint256 _orderId,
+        address _user,
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
+        uint256 _amountGive
+    ) internal {
+        uint256 _feeAmount = (_amountGet * feePercent) / 100;
+
+        tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender] - (_amountGet + _feeAmount);
+        tokens[_tokenGet][_user] = tokens[_tokenGet][_user] + _amountGet;
+
+        tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount] + _feeAmount;
+
+        tokens[_tokenGive][_user] = tokens[_tokenGive][_user] - _amountGive;
+        tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender] + _amountGive;
+
+        emit Trade(
+            _orderId,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            _user,
+            block.timestamp
+        );
+
+
     }
 
 }
